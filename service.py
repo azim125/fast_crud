b31263944b51d70543b01f075933aea1ef118baf
azim125 <mmmdovzim76@gmail.com>
2024-10-10 14:37:21 +0300

    changeddddd

--- service.py
+++ service.py
@@ -32,5 +32,5 @@ def delete_user_in_db(*, data: UserDeleteSchema,db: Session):
     if not user_in_db:
         raise UserNotFoundException()
     db.delete(user_in_db)
-    #0db.commit()
+    db.commit()
     return {"msg":"user is deleted"}
